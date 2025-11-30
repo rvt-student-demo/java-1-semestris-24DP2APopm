28cb40ea97d22c42bb814b10cd5ae19b350adf0e
24DP2APopm <A240367AP@rvt.lv>
2025-11-30 18:41:58 +0000

    Uzdevums "Advanced Astrology", Java MOOC 2-4

---
+++ src/main/java/rvt/AdvancedAstrology.java
@@ -0,0 +1,47 @@
+package rvt;
+
+import java.util.*;
+
+public class AdvancedAstrology {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printStars(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        int i = 1;
+        int j = height - 1;
+
+        while (i <= height * 2 && j >= 0) {
+            printSpaces(j);
+            printStars(i);
+            i = i + 2;
+            j--;
+            if (j == -1) {
+                printSpaces(height - 2);
+                System.out.println("***");
+                printSpaces(height - 2);
+                System.out.println("***");
+            }
+        }
+    }
+}
